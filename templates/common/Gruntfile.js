b2cb5cf2279692c98c76bf0fa5510a4459cc381b
Diego Netto <diegormnetto@gmail.com>
2014-09-07 18:21:20 -0500

    fix(sass): Ensure CSS or SCSS files are properly created. Change wiredep config. Closes #99

--- templates/common/Gruntfile.js
+++ templates/common/Gruntfile.js
@@ -173,9 +173,6 @@ module.exports = function (grunt) {
 
     // Automatically inject Bower components into the app
     wiredep: {
-      options: {
-        cwd: '<%%= yeoman.app %>'
-      },
       app: {
         src: ['<%%= yeoman.app %>/index.html'],
         ignorePath:  /\.\.\//
