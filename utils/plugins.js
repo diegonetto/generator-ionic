f77c8f5d1a7270217f794bca528739ec121efb63
Sidney Bofah <sidney.bofah@googlemail.com>
2015-01-26 02:00:23 +0100

    Reordered default plugins and enabled only the minimum set required by Ionic v1/Beta14.

--- utils/plugins.js
+++ utils/plugins.js
@@ -4,29 +4,33 @@ module.exports = {
     name: 'plugins',
     message: 'Which Cordova plugins would you like to include?',
     choices: [{
-      value: 'org.apache.cordova.console',
-      name: 'org.apache.cordova.console',
-      checked: true
-    }, {
       value: 'org.apache.cordova.device',
       name: 'org.apache.cordova.device',
       checked: true
     }, {
-      value: 'org.apache.cordova.network-information',
-      name: 'org.apache.cordova.network-information',
-      checked: false
+      value: 'org.apache.cordova.console',
+      name: 'org.apache.cordova.console',
+      checked: true
+    }, {
+      value: 'com.ionic.keyboard',
+      name: 'com.ionic.keyboard',
+      checked: true
     }, {
       value: 'org.apache.cordova.splashscreen',
       name: 'org.apache.cordova.splashscreen',
-      checked: true
+      checked: false
+    }, {
+      value: 'org.apache.cordova.statusbar',
+      name: 'org.apache.cordova.statusbar',
+      checked: false
     }, {
       value: 'org.apache.cordova.battery-status',
       name: 'org.apache.cordova.battery-status',
       checked: false
     }, {
-      value: 'org.apache.cordova.statusbar',
-      name: 'org.apache.cordova.statusbar',
-      checked: true
+      value: 'org.apache.cordova.network-information',
+      name: 'org.apache.cordova.network-information',
+      checked: false
     }, {
       value: 'org.apache.cordova.device-motion',
       name: 'org.apache.cordova.device-motion',
@@ -38,43 +42,43 @@ module.exports = {
     }, {
       value: 'org.apache.cordova.geolocation',
       name: 'org.apache.cordova.geolocation',
-      checked: true
+      checked: false
     }, {
       value: 'org.apache.cordova.camera',
       name: 'org.apache.cordova.camera',
-      checked: true
+      checked: false
     }, {
       value: 'org.apache.cordova.media-capture',
       name: 'org.apache.cordova.media-capture',
-      checked: true
+      checked: false
     }, {
       value: 'org.apache.cordova.media',
       name: 'org.apache.cordova.media',
-      checked: true
+      checked: false
     }, {
       value: 'org.apache.cordova.file',
       name: 'org.apache.cordova.file',
-      checked: true
+      checked: false
     }, {
       value: 'org.apache.cordova.file-transfer',
       name: 'org.apache.cordova.file-transfer',
-      checked: true
+      checked: false
     }, {
       value: 'org.apache.cordova.dialogs',
       name: 'org.apache.cordova.dialogs',
-      checked: true
+      checked: false
     }, {
       value: 'org.apache.cordova.vibration',
       name: 'org.apache.cordova.vibration',
-      checked: true
+      checked: false
     }, {
       value: 'org.apache.cordova.contacts',
       name: 'org.apache.cordova.contacts',
-      checked: true
+      checked: false
     }, {
       value: 'org.apache.cordova.inappbrowser',
       name: 'org.apache.cordova.inappbrowser',
-      checked: true
+      checked: false
     }]
   }]
 };
